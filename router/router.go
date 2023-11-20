617086a60859888545dcc85bd6c70ac7c4766dbd
Nora Trapp <me@trappdesign.net>
2023-11-19 18:28:59 -0800

    Use TLS with certbot, but enforce api requests come from local IP address

--- router/router.go
+++ router/router.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"time"
@@ -15,7 +16,10 @@ import (
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/crypto/acme/autocert"
 )
+
+var localInternetAddress = os.Getenv("LOCAL_INTERNET_ADDRESS")
 
 func RunRouter() {
 	signingKey := os.Getenv("ACCESS_CONTROL_SIGNING_KEY")
@@ -23,18 +27,27 @@ func RunRouter() {
 		log.Fatal("signing key is missing")
 	}
 
+	if localInternetAddress == "" {
+		log.Fatal("local internet address is missing")
+	}
+
 	door := door.New()
 
 	e := echo.New()
 	e.HideBanner = true
+	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
 
+	e.GET("/", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
+
 	e.GET("/api/v1/status", func(c echo.Context) error {
 		return jsonResponse(c, http.StatusOK, "door control available")
-	})
+	}, requireLocalNetworkMiddleware)
 
 	e.POST("/api/v1/unlock", func(c echo.Context) error {
 		body, err := io.ReadAll(c.Request().Body)
@@ -64,13 +77,33 @@ func RunRouter() {
 		}
 
 		return jsonResponse(c, http.StatusOK, "access granted")
-	}, echojwt.WithConfig(echojwt.Config{
+	}, requireLocalNetworkMiddleware, echojwt.WithConfig(echojwt.Config{
 		SigningKey: []byte(signingKey),
 	}))
 
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.StartAutoTLS(":8443"))
 }
 
 func jsonResponse(c echo.Context, code int, message string) error {
 	return c.JSON(code, map[string]string{"message": message})
+}
+
+func requireLocalNetworkMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// Get the remote address from the request
+		remoteAddr := c.Request().RemoteAddr
+
+		// Parse the IP address
+		ip, _, err := net.SplitHostPort(remoteAddr)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to parse IP address")
+		}
+
+		if ip != localInternetAddress {
+			return jsonResponse(c, http.StatusForbidden, "requests not allowed from remote hosts")
+		}
+
+		// Continue to the next middleware or route handler
+		return next(c)
+	}
 }
